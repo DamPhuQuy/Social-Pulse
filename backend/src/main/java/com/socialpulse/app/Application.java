13c335cc5bdb9d98b4ca3d61b486d6d1c5f57076
DamPhuQuy <phuquydam06@gmail.com>
2026-05-18 22:24:12 +0700

    set it in security config

--- backend/src/main/java/com/socialpulse/app/Application.java
+++ backend/src/main/java/com/socialpulse/app/Application.java
@@ -2,7 +2,6 @@ package com.socialpulse.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 public class Application {
